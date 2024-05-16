0583b47735a8948cf16536bd0d5cfc0f4eb0f0a2
Sarunas <sarugen12@gmail.com>
2024-05-16 13:41:43 +0200

    Add project files.

---
+++ PirmaPaskaita/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Transactions;
+
+namespace PirmaPaskaita
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string kintamasis = "Sveiki studentai!";
+
+            var x = kintamasis.Where(e => e.Equals("e"));
+            
+        }
+    }
+}
